c6adc7768b0bbe55458ac2b688bebbc709a76d0b
Kam <kammif1990@gmail.com>
2016-01-18 20:23:25 +0500

    Update comments_helper.rb

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
 	
 	def rate_comment_button(comment, user, action)
-    abc = rate_comment_path(comment, format: :json)
+    rate_value = rate_comment_path(comment, format: :json)
     if action == :like
       updown = 'up'
     else
@@ -9,7 +9,7 @@ module CommentsHelper
       flip   = 'fa-flip-horizontal'
     end
     %Q{
-      <span class="post-rate comment-#{action} glyphicon glyphicon-thumbs-#{updown} #{flip}" data-url="#{abc}" data-act="#{action}"></span>
+      <span class="post-rate comment-#{action} glyphicon glyphicon-thumbs-#{updown} #{flip}" data-url="#{rate_value}" data-act="#{action}"></span>
     }.html_safe #возвращаемый html элемент (кнопка)
   end
 
