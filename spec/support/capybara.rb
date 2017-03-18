22e9c63398ec73cc38a633886b1f19c5e838f2da
mpakus <mrak69@gmail.com>
2017-03-18 11:38:38 +0700

    rspec + capybara fixed and run (but with errors)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,14 @@
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+# require 'capybara/poltergeist'
+# Capybara.javascript_driver = :poltergeist
+#
+# Capybara.register_driver :poltergeist do |app|
+#   Capybara::Poltergeist::Driver.new(app, timeout: 360)
+# end
 
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, timeout: 360)
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :firefox,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
+  )
 end
