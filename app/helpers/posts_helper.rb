76b589737bda4ed17f53fd84ef3dbadbcb75c758
mpakus <mrak69@gmail.com>
2017-03-26 10:29:50 +0700

    Tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,7 +6,7 @@ module PostsHelper
 
     out = []
     post.tags.each do |tag|
-      out << link_to(tag.name, posts_path(tag: tag.name), class: 'tag label label-info')
+      out << link_to(tag.name, posts_path(tag: tag.name), class: 'tag label label-info label-xs')
     end
     out.join('').html_safe
   end
